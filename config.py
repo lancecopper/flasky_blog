1cf4842d1a681e82a1b63cdf28dd03dc0c9198bd
lancecopper <she47637370@gamil.com>
2016-04-11 13:18:11 +0800

    config mod

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ class ProductionConfig(Config):
             if getattr(cls, "MAIL_USE_TLS", None):
                 secure = ()
         mail_handler = SMTPHandler(
-            mailhost=(cls,MAIL_SERVER, cls.MAIL_PORT)
+            mailhost=(cls,MAIL_SERVER, cls.MAIL_PORT),
             fromaddr=cls.FLASKY_MAIL_SENDER,
             toaddr=[cls.FLASKY_ADMIN],
             subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
